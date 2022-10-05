b63abde0c251a5b139cd4531047604017386e318
LucLind <luclindemann@gmail.com>
2022-10-05 14:13:18 +0200

    LF08PS-15
    Leeres Java Spring Projekt
    Maven Dependencies aus dem Webshop Projekt copypasted

---
+++ projectmanagement/src/test/java/de/szut/project/ProjectmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.szut.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
