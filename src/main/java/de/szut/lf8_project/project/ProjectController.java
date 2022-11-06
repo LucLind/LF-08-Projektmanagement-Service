3f49e1587277c133dbcb25170adb892f0d4d2271
josephak1 <100535050+josephak1@users.noreply.github.com>
2022-11-06 12:57:48 +0100

    LF08PS-55
    Get All Projects Endpunkt

--- src/main/java/de/szut/lf8_project/project/ProjectController.java
+++ src/main/java/de/szut/lf8_project/project/ProjectController.java
@@ -6,13 +6,12 @@ import de.szut.lf8_project.project.dto.AddProjectDto;
 import de.szut.lf8_project.project.dto.GetProjectDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequestMapping("project")
 @RestController
@@ -54,6 +53,21 @@ public class ProjectController {
         GetProjectDto response = projectMapper.MapProjectToGetProjectDto(entity);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+    /**
+     * Get Endpunkt für alle Projekte
+     * @return
+     */
+    @GetMapping
+    public List<GetProjectDto> findAll(){
+        return this.service
+                .readAll()      // get all project
+                .stream()   // convert to stream
+                .map(projectMapper::MapProjectToGetProjectDto)   // map each project to a GetProjectDTO
+                .collect(Collectors.toList());   // collect the results in a list
+    }
+
+
+
 
 
 }
