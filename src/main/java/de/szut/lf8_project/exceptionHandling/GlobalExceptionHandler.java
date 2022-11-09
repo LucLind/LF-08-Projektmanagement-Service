5ded24af2b1b79ec81361eed0eb62c34656e1623
josephak1 <100535050+josephak1@users.noreply.github.com>
2022-11-09 11:05:07 +0100

    LF08PS-68
    ErrorDetails Umbenennung in DTO

--- src/main/java/de/szut/lf8_project/exceptionHandling/GlobalExceptionHandler.java
+++ src/main/java/de/szut/lf8_project/exceptionHandling/GlobalExceptionHandler.java
@@ -3,10 +3,8 @@ package de.szut.lf8_project.exceptionHandling;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,8 +20,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        ErrorDetailsDTO errorDetailsDTO = new ErrorDetailsDTO(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetailsDTO, HttpStatus.NOT_FOUND);
     }
 
 
