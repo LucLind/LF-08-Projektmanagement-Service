6a58ec28ac72439c34e0d9b58fb46fb88bd6a93b
LucLind <luclindemann@gmail.com>
2022-11-14 06:13:43 +0100

    LF08PS-42
    Dummy Endpunkte für Customer

---
+++ src/main/java/de/szut/lf8_project/customer/dto/GetCustomerDTO.java
@@ -0,0 +1,14 @@
+package de.szut.lf8_project.customer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetCustomerDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
--- src/main/java/de/szut/lf8_project/qualification/dto/EmployeesForAQualificationDTO.java
+++ src/main/java/de/szut/lf8_project/qualification/dto/EmployeesForAQualificationDTO.java
@@ -1,7 +1,9 @@
 package de.szut.lf8_project.qualification.dto;
 
 import de.szut.lf8_project.employee.dto.external.EmployeeNameDataDTO;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
