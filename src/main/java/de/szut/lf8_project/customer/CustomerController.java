8c599b43f688e94f16e3f4c0ba119e9daefe5303
Evo77106 <daniel.nanninga@artundweise.de>
2022-11-16 09:35:41 +0100

    Update CustomerController.java

--- src/main/java/de/szut/lf8_project/customer/CustomerController.java
+++ src/main/java/de/szut/lf8_project/customer/CustomerController.java
@@ -16,17 +16,30 @@ import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Customer Controller class
+ */
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
     private CustomerService service;
     private CustomerMapping customerMapping;
 
+    /**
+     * constructor for CustomerController
+     * @param service the service
+     * @param customerMapping the customer mapping
+     */
     public CustomerController(CustomerService service, CustomerMapping customerMapping){
         this.customerMapping = customerMapping;
         this.service = service;
     }
 
+    /**
+     * Dummy-methog for posting customers
+     * @param dto the post customer DTO
+     * @return Response entity OK for test purposes
+     */
     @Operation(summary = "erstellt einen neuen Kunden-Dummy")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Kunde erfolgreich erstellt- Dummy",
@@ -39,10 +52,14 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<GetCustomerDTO> postCustomer(@RequestBody @Valid PostCustomerDTO dto){
         // Kunden werden noch nicht implementiert daher hier nur als dummy ein OK zurückgeben.
-        var response = new GetCustomerDTO(0L, "WARNING: CUSTOMERS ARE STILL NOT IMPLEMENTED", "YOU WILL ONLY RECIEVE TEST DATA.");
+        var response = new GetCustomerDTO(0L, "WARNING: CUSTOMERS ARE STILL NOT IMPLEMENTED", "YOU WILL ONLY RECEIVE TEST DATA.");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * Method for getting all Customers (Testing purposes / dummy data)
+     * @return Response entity OK for test purposes
+     */
     @Operation(summary = "Alle Kunden auslesen-Dummy")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Alle Kunden erfolgreich ausgelesen- Dummy",
@@ -61,6 +78,12 @@ public class CustomerController {
                 "YOU WILL ONLY RECIEVE TEST DATA."));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    /**
+     * Method for getting a specific customer by its id (Testing purposes / dummy data)
+     * @param id the customer id
+     * @return Response entity OK for test purposes
+     */
     @Operation(summary = "Alle Projekte eines Kunden auslesen-Dummy")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Alle Projekte eines Kunden erfolgreich ausgelesen- Dummy",
@@ -73,7 +96,7 @@ public class CustomerController {
     @GetMapping("/{id}")
     public ResponseEntity<GetCustomerDTO> getCustomerById(@PathVariable Long id){
         // Kunden werden noch nicht implementiert daher hier nur als dummy ein OK zurückgeben.
-        var response = new GetCustomerDTO(0L, "WARNING: CUSTOMERS ARE STILL NOT IMPLEMENTED", "YOU WILL ONLY RECIEVE TEST DATA.");
+        var response = new GetCustomerDTO(0L, "WARNING: CUSTOMERS ARE STILL NOT IMPLEMENTED", "YOU WILL ONLY RECEIVE TEST DATA.");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
